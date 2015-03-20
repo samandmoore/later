be70aadebf186257233aa5c19f577ec9d82f334c
Sam Moore <samandmoore@gmail.com>
2015-03-20 00:03:59 -0400

    the meat

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    webpack: {
+      dev: {
+        entry: "./src/index.js",
+        sourceMapFilename: "index.js.map",
+        output: {
+          path: "public/assets/javascript/",
+          filename: "index.js",
+          pathinfo: true
+        },
+        module: {
+          loaders: [
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+          ]
+        },
+        resolve: {
+          modulesDirectories: ['node_modules', 'vendor']
+        },
+        cache: true,
+        watch: true,
+        debug: true
+      }
+    },
+
+    watch: {
+      grunt: {
+        files: ['Gruntfile.js']
+      },
+      assets: {
+        files: ['src/**/*.js'],
+        tasks: ['webpack:dev']
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['webpack', 'watch']);
+}
---
+++ src/app.js
@@ -0,0 +1,5 @@
+import sm from 'backbone.supermodel';
+
+let app = sm.Model.extend();
+
+export default app;
